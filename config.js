fce37e5c0fd41c62a072f70bd706e1ea1916e281
Ravindra <rk2671353@gmail.com>
2024-06-04 18:12:18 +0530

    config.js

--- config.js
+++ config.js
@@ -1,2 +1,13 @@
-const mongoose = require('mongoose'); 
-mongoose.connect("mongodb://localhost:27017/bookstore");
+ 
+const mongoose = require('mongoose');
+const dbHOST = process.env.DBHOST;
+
+mongoose.connect(dbHOST)
+    .then(() => {
+        console.log('MongoDB Connnected...')
+    }).catch((err) => {
+        console.log('Error while Mongo Conn..', err);
+    })
+
+// const mongoose = require('mongoose');
+// mongoose.connect("mongodb://localhost:27017/bookstore");
